526c7405be5fac7ea2249e158b82b930b864bd0e
Airidas Kutra <airidas.kutra2@gmail.com>
2018-12-23 21:48:28 +0200

    Changed maps to const

--- Engine/ConversionMaps.h
+++ Engine/ConversionMaps.h
@@ -5,20 +5,20 @@
 #include <unordered_map>
 
 // String -> DamageType map
-std::unordered_map<std::string, DamageType> damageTypeMap =
+const std::unordered_map<std::string, DamageType> damageTypeMap =
 {
     { "physical", D_PHYSICAL },
-    { "air"        , D_AIR },
-    { "earth"    , D_EARTH },
-    { "fire"    , D_FIRE },
-    { "water"    , D_WATER },
-    { "holy"    , D_HOLY },
-    { "demonic"    , D_DEMONIC },
-    { "p"        , D_PHYSICAL },
-    { "a"        , D_AIR },
-    { "e"        , D_EARTH },
-    { "f"        , D_FIRE },
-    { "w"        , D_WATER },
-    { "h"        , D_HOLY },
-    { "d"        , D_DEMONIC }
+    { "air"     , D_AIR      },
+    { "earth"   , D_EARTH    },
+    { "fire"    , D_FIRE     },
+    { "water"   , D_WATER    },
+    { "holy"    , D_HOLY     },
+    { "demonic" , D_DEMONIC  },
+    { "p"       , D_PHYSICAL },
+    { "a"       , D_AIR      },
+    { "e"       , D_EARTH    },
+    { "f"       , D_FIRE     },
+    { "w"       , D_WATER    },
+    { "h"       , D_HOLY     },
+    { "d"       , D_DEMONIC  }
 };
