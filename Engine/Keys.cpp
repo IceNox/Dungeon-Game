dc5d20fcb577d5755ad344ff8c6f9920fc2c7429
Icebox <mantas_kandratavicius@yahoo.com>
2019-01-10 00:32:31 +0100

    "0" Missing in SmartText whitelist fixed

--- Engine/Keys.cpp
+++ Engine/Keys.cpp
@@ -60,7 +60,7 @@ namespace kb
 
     // SmartText class implementation
     const std::string SmartText::spaces  = " ";
-    const std::string SmartText::numbers = "123456789";
+    const std::string SmartText::numbers = "0123456789";
     const std::string SmartText::letters = "abcdefghijklmnopqrstuvwxyz";
     const std::string SmartText::special = "";
 
