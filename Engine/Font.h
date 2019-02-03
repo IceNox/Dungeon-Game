87aefa7467856e9134851f3d436b8315a439c752
Airidas Kutra <airidas.kutra2@gmail.com>
2019-02-03 22:04:40 +0200

    Reworked font class and storage

--- Engine/Font.h
+++ Engine/Font.h
@@ -8,8 +8,11 @@
 class Font
 {
 public:
+    std::string name;
     std::vector<Sprite> character;
-    std::vector<int>    characterW;
+    std::vector<int> characterW;
+    std::vector<int> characterH;
+    int kerning;
 
     Font();
 
